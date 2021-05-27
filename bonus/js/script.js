b5bd94af63e56c578079397a6a37a75dd07acbe0
MyUsername <emanuele.marini37@gmail.com>
2021-05-28 00:30:40 +0200

    bozza avanza

--- bonus/js/script.js
+++ bonus/js/script.js
@@ -1,8 +1,13 @@
 document.writeln("Con writeln scrivo nella pagina");
 
-alert("sono un alert, ho la precedenza assoluta e ti impedisco di operare in pagina, tsè ");
+ alert("sono un alert, ho la precedenza assoluta e ti impedisco di operare in pagina, tsè ");
 
 prompt("come alert blocco la pagina, esigo una risposta e il valore è sempre STRINGA");   
 
 var userName = prompt("come ti chiami=?");
 document.getElementById('name').innerHTML = userName;
+
+
+var year = prompt('inserisci la tua data di nascita');
+var age = 2021 - year;
+document.getElementById('age-result').innerHTML = "La tua età è "+ age;
