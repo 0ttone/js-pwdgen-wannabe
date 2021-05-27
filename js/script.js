d35d440ce07b2df4abc1bda3e7dac67e9212e4ed
MyUsername <emanuele.marini37@gmail.com>
2021-05-27 17:32:05 +0200

    bozza ok

---
+++ js/script.js
@@ -0,0 +1,8 @@
+
+document.getElementById("pwdID").innerHTML = "Il peggior generatore di password possibile"
+
+var name = prompt("Come ti chiami?");
+var lastName = prompt("E il tuo cognome quale è?");
+var prefColour = prompt("Il tuo colore preferito?");
+
+document.getElementById("password").innerHTML = name + lastName + prefColour + "21"
